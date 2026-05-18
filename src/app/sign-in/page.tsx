7a5bb13b4439302dd748df7836722e84418f4569
Julia Sales <juliasalesc@outlook.com>
2026-05-18 15:16:58 -0300

    icons and imports added

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,20 +1,36 @@
+import Image from 'next/image';
+import WorldIcon from '@/assets/world-icon.png';
+import CloseIcon from '@/assets/close-icon.png';
+
 export default function SignIn() {
   return (
     <div className="flex h-screen">
-      <aside className="bg-[#030204] flex-1 px-9 py-9">
+      <aside className="bg-[#030204] flex-1 px-9 py-9 overflow-hidden">
         <div>
           <h2 className="bg-white text-[18px] text-black h-10 w-10 rounded-[7px] flex justify-center items-end font-bold">
             itaú
           </h2>
         </div>
 
-        <div className="flex justify-center items-center h-20 w-20 rounded-[7px] bg-orange-500">
-            <p></p>
+        <div className="flex justify-center items-center h-full">
+          <div className="absolute w-96 h-96 bg-[#FF6202]/50 rounded-full blur-[120px]" />
+          <div className="relative w-40 h-40 bg-black rounded-xl border border-[#FF6202]/50" />
         </div>
       </aside>
 
-      <main className="bg-white flex-1">
-        <h1>LOGIN</h1>
+      <main className="bg-white flex-1 p-9 text-black">
+        <div className='flex justify-between'>
+          <div className='flex gap-2'>
+          <Image src={WorldIcon} alt='Ícone mundo' className='h-5 w-5'/>
+          <p>BR</p>
+        </div>
+
+        <div className='flex gap-2'>
+          <Image src={CloseIcon} alt='Ícone fechar' className='h-5 w-5'/>
+          <p>Close</p>
+        </div>
+        </div>
+        
       </main>
     </div>
   );
